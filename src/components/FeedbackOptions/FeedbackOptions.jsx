7b7272313b3f0f23bfcacd8b874bf4a60d018b43
Angela Potapchuk <angela.harkov@gmail.com>
2023-05-18 16:26:16 +0300

    teat

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -26,6 +26,7 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   );
 };
 
+
 // FeedbackOptions.propTypes = {
 //   options: PropTypes.arrayOf(PropTypes.string),
 //   onLeaveFeedback: PropTypes.func,
